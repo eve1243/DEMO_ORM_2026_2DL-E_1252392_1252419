f68b95810a648c6a9c7105a33ba721431a82faf2
eve1243 <Everlyn.Njeri@stud.fh-campuswien.ac.at>
2026-03-04 16:26:07 +0000

    Add Console and DateTime utility classes, and implement MainMenu for user interaction

---
+++ Demo_ORM/src/main/java/isep/eapli/demo_orm/DemoORM.java
@@ -0,0 +1,7 @@
+package isep.eapli.demo_orm;
+import isep.eapli.demo_orm.presentation.MainMenu;
+public class DemoORM {
+    public static void main(String[] args) {
+        MainMenu.mainLoop();
+    }
+}
